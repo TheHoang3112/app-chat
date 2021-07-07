fef2881d3a5c94e52a044e67bbc5972e0c45949d
Hoàng <thehoang3112u@gmail.com>
2021-07-07 08:41:05 +0700

    3.4  Delete and Update Nweet

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,19 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { dbService } from "fbase";
+import Nweet from "components/Nweet";
 
 const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
-    // const getNweets = async () => {
-    //     const dbNweets = await dbService.collection("nweets").get();
-    //     dbNweets.forEach((document) => {
-    //         const nweetObject = {
-    //             ...document.data(),
-    //             id: document.id,
-    //         };
-    //         setNweets((prev) => [nweetObject, ...prev]);
-    //     });
-    // };
     useEffect(() => {
         dbService.collection("nweets").onSnapshot((snapshot) => {
             const nweetArray = snapshot.docs.map((doc) => ({
@@ -53,9 +44,11 @@ const Home = ({ userObj }) => {
             </form>
             <div>
                 {nweets.map((nweet) => (
-                    <div key={nweet.id}>
-                        <h4>{nweet.text}</h4>
-                    </div>
+                    <Nweet
+                        key={nweet.id}
+                        nweetObj={nweet}
+                        isOwner={nweet.creatorId === userObj.uid}
+                    />
                 ))}
             </div>
         </div>
